c38c99efeb78319533327ceef83d71b0c9dc0190
Kristy\KristyL <t113C53062@ntut.org.tw>
2025-03-26 17:37:42 +0800

    很瞎的code

---
+++ Services/LoginService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co_Practice.Services
+{
+    internal class LoginService
+    {
+        public LoginService(string username, string password)
+        {
+
+        }
+        public void Login(string username, string password)
+        {
+            if (username == password)
+            {
+                Console.WriteLine("登入成功");
+            }
+            else
+            {
+                Console.WriteLine("登入失敗");
+            }
+        }
+    }
+}
